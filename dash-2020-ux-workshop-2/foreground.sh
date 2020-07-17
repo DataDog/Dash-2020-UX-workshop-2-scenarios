77f6f905cc97c044d687468b35bd250a9b7f2882
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-07-17 05:02:38 +0200

    :construction: wip

--- dash-2020-ux-workshop-2/foreground.sh
+++ dash-2020-ux-workshop-2/foreground.sh
@@ -2,9 +2,16 @@
 
 echo "Setup..."
 
-while [ ! -f "~/done-setup" ]; do
-  sleep 1
-done
 
-rm ~/done-setup
-cd ~/ecommerce-observability
+mkdir ~/ecommerce-observability
+git clone https://github.com/etnbrd/ecommerce-observability ~/ecommerce-observability
+cd ~/ecommerce-observability && git checkout no-instrumentation
+cp -r hooks .git/hooks
+# touch ~/done-setup
+
+# while [ ! -f "~/done-setup" ]; do
+#   sleep 1
+# done
+
+# rm ~/done-setup
+# cd ~/ecommerce-observability
--- dash-2020-ux-workshop-2/setup.sh
+++ dash-2020-ux-workshop-2/setup.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-mkdir ~/ecommerce-observability
-git clone https://github.com/etnbrd/ecommerce-observability ~/ecommerce-observability
-cd ~/ecommerce-observability && git checkout no-instrumentation
-cp -r hooks .git/hooks
-touch ~/done-setup
