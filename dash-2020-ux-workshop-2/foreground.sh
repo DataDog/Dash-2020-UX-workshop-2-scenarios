e00eaac8783a345c6d28f33364585cc5dca3a91e
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-07-17 06:15:33 +0200

    :construction: wip

--- dash-2020-ux-workshop-2/foreground.sh
+++ dash-2020-ux-workshop-2/foreground.sh
@@ -7,7 +7,7 @@ git init --bare remote # Prepare a fake remote to push to
 git clone https://github.com/etnbrd/ecommerce-workshop.git ~/ecommerce-observability
 cd ~/ecommerce-observability
 git remote add deploy ~/remote # Add the fake remote
-cp hooks/* .git/hooks/ # Setup the fake CI/CD
+cp scripts/pre-push .git/hooks/ # Setup the fake CI/CD
 
 git config --global user.email "relevant-titmouse@dash2020.datadoghq.com"
 git config --global user.name "relevant titmouse"
