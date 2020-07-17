2751ad5c37f4a42d3de9999b09b802f8dc42c629
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-07-17 13:57:44 +0200

    :construction: wip

--- dash-2020-ux-workshop-2/foreground.sh
+++ dash-2020-ux-workshop-2/foreground.sh
@@ -2,25 +2,25 @@
 
 echo "Setup..."
 
-# git init --bare remote # Prepare a fake remote to push to
+git init --bare remote # Prepare a fake remote to push to
 
-# git clone https://github.com/etnbrd/ecommerce-workshop.git ~/ecommerce-observability
-# cd ~/ecommerce-observability
-# git remote add deploy ~/remote # Add the fake remote
-# cp scripts/pre-push .git/hooks/ # Setup the fake CI/CD
+git clone https://github.com/etnbrd/ecommerce-workshop.git ~/ecommerce-observability
+cd ~/ecommerce-observability
+git remote add deploy ~/remote # Add the fake remote
+cp scripts/pre-push .git/hooks/ # Setup the fake CI/CD
 
-# git config --global user.email "relevant-titmouse@dash2020.datadoghq.com"
-# git config --global user.name "relevant titmouse"
+git config --global user.email "relevant-titmouse@dash2020.datadoghq.com"
+git config --global user.name "relevant titmouse"
 
 # ./scripts/build.sh
 
-mkdir /ecommerce-observability
-git clone https://github.com/burningion/ecommerce-observability /ecommerce-observability
-cd /ecommerce-observability && git checkout no-instrumentation
+# mkdir /ecommerce-observability
+# git clone https://github.com/burningion/ecommerce-observability /ecommerce-observability
+# cd /ecommerce-observability && git checkout no-instrumentation
 
-docker pull postgres:11-alpine
-docker pull burningion/ecommerce-spree-ads:latest
-docker pull burningion/ecommerce-spree-observability:cpu-workaround
-docker pull burningion/ecommerce-spree-discounts:latest
+# docker pull postgres:11-alpine
+# docker pull burningion/ecommerce-spree-ads:latest
+# docker pull burningion/ecommerce-spree-observability:cpu-workaround
+# docker pull burningion/ecommerce-spree-discounts:latest
 
 echo "Done"
