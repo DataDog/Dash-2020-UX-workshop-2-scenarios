bcdf9175f90a71208605ba45f204237465d7e47d
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-07-17 05:35:06 +0200

    :construction: wip

--- dash-2020-ux-workshop-2/foreground.sh
+++ dash-2020-ux-workshop-2/foreground.sh
@@ -9,9 +9,7 @@ cd ~/ecommerce-observability
 git remote add deploy ~/remote # Add the fake remote
 cp hooks/* .git/hooks/ # Setup the fake CI/CD
 
-apt-get install -yq petname
-NAME=$(petname)
-git config --globale user.email="${NAME}@dash2020.datadoghq.com"
-git config --globale user.name="${NAME/-/ }"
+git config --globale user.email="relevant-titmouse@dash2020.datadoghq.com"
+git config --globale user.name="relevant titmouse"
 
 echo "Done"
