aab9000fe8b155be6e7fd9176a7715e7d6679fdb
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-07-17 04:44:59 +0200

    :construction: wip

--- dash-2020-ux-workshop-2/setup.sh
+++ dash-2020-ux-workshop-2/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir ~/ecommerce-observability
-git clone https://github.com/burningion/ecommerce-observability ~/ecommerce-observability
+git clone https://github.com/etnbrd/ecommerce-observability ~/ecommerce-observability
 cd ~/ecommerce-observability && git checkout no-instrumentation
+cp -r hooks .git/hooks
